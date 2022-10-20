20e2db378c5ec50aba64df23a9ed79c8481057a5
SySaskiaa <99782751+SySaskiaa@users.noreply.github.com>
2022-10-20 23:43:55 +0700

    Add files via upload

---
+++ No3 Interface prak6.php
@@ -0,0 +1,16 @@
+<!-- Nama   : Syarifah Saskia
+     Nim    : 21091397012 
+     Kelas  : 2021B -->
+
+<?php 
+
+interface Flyer {
+    public function takeOff();
+    public function land();
+    public function fly();
+}
+
+interface Sailer {
+    public function dock();
+    public function cruise();
+}
